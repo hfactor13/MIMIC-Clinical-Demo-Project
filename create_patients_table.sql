7e8b0572ec0484c90cde41293a3e923db0bfcd7d
Henry Cuzco <40706933+uiucmeche1317@users.noreply.github.com>
2025-03-12 15:40:53 -0500

    Initial commit

---
+++ create_patients_table.sql
@@ -0,0 +1,30 @@
+-- Combines admissions and patients table
+CREATE OR REPLACE TABLE pat_ad_tbl AS 
+SELECT
+	a.row_id,
+	a.subject_id,
+	a.hadm_id,
+	a.admission_type,
+	a.admission_location,
+	a.discharge_location,
+	a.insurance,
+	a.language,
+	a.religion,
+	a.marital_status,
+	a.ethnicity,
+	a.diagnosis,
+	p.gender,
+	p.dob,
+	p.dod
+FROM
+	admissions a
+JOIN patients p ON
+	p.subject_id = a.subject_id;
+
+-- Adds age column
+ALTER TABLE pat_ad_tbl ADD COLUMN age int;
+UPDATE pat_ad_tbl SET age = (EXTRACT(DAY FROM (dod - dob))::FLOAT) / 365.25;
+
+-- Removes dob and dod columns
+ALTER TABLE pat_ad_tbl DROP COLUMN dob;
+ALTER TABLE pat_ad_tbl DROP COLUMN dod;
