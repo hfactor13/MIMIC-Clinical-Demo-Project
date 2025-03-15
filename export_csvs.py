8f8d79384ada848834d2763760d77e8bc0831c1f
Henry Cuzco <40706933+uiucmeche1317@users.noreply.github.com>
2025-03-14 20:11:48 -0500

    Turned export_csvs.py and load_csvs.py into modules

--- export_csvs.py
+++ export_csvs.py
@@ -1,10 +1,14 @@
-from pathlib import Path
-import pandas as pd
-import duckdb as db
+def export_csv_files():
+    from pathlib import Path
+    import pandas as pd
+    import duckdb as db
 
-output_dir = Path.cwd() / "output"
-desired_tables = ["pat_tbl_combo", "procedure_tbl_combo", "icustays"]
+    output_dir = Path.cwd() / "output"
+    desired_tables = ["pat_tbl_combo", "procedure_tbl_combo", "icustays"]
 
-with db.connect("mimic-clinical-demo.db") as con:
-    for tbl_name in desired_tables:
-        con.table(tbl_name).to_csv(output_dir / f"{tbl_name}.csv", sep = ",", index = False)
+    with db.connect("mimic-clinical-demo.db") as con:
+        for tbl_name in desired_tables:
+            con.table(tbl_name).to_csv(output_dir / f"{tbl_name}.csv", sep = ",", index = False)
+
+if __name__ == "__main__":
+    export_csv_files()
--- load_csvs.py
+++ load_csvs.py
@@ -1,19 +1,23 @@
-# This script loads CSV files into tables in a duckdb database
-from pathlib import Path
-import duckdb as db
+def load_csv_files():
+    # This script loads CSV files into tables in a duckdb database
+    from pathlib import Path
+    import duckdb as db
 
-# Defines the working directory and gets all the CSV files
-working_dir = Path.cwd() / "mimic-clinical-demo"
-csv_file_path = list(working_dir.glob("*.csv"))
-csv_file_path
+    # Defines the working directory and gets all the CSV files
+    working_dir = Path.cwd() / "mimic-clinical-demo"
+    csv_file_path = list(working_dir.glob("*.csv"))
+    csv_file_path
 
-# Get the table names
-db_table_names = [file.stem.lower() for file in csv_file_path]
-db_table_names
+    # Get the table names
+    db_table_names = [file.stem.lower() for file in csv_file_path]
+    db_table_names
 
-# Defines the duckdb database and saves all the CSVs to the database as separate tables
-con = db.connect("mimic-clinical-demo.db")
-for table, file_path in zip(db_table_names, csv_file_path):
-    with db.connect("mimic-clinical-demo.db") as con:
-        con.execute(f"""CREATE OR REPLACE TABLE {table} AS 
+    # Defines the duckdb database and saves all the CSVs to the database as separate tables
+    con = db.connect("mimic-clinical-demo.db")
+    for table, file_path in zip(db_table_names, csv_file_path):
+        with db.connect("mimic-clinical-demo.db") as con:
+            con.execute(f"""CREATE OR REPLACE TABLE {table} AS 
                         SELECT * FROM read_csv('{file_path}', delim=',', header=True, quote='"', escape='"', ignore_errors=True); """)
+
+if __name__ == "__main__":
+    load_csv_files()
